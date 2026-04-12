3f5018d995094272feaf26e0d20dd04c9c8d306d
JuliotLeonce <juliotleonce123@gmail.com>
2026-04-12 17:22:31 +0300

    edit(xmemctl): add macro for scope to make a boundary on memory liberation

--- include/xmemctl.h
+++ include/xmemctl.h
@@ -6,6 +6,13 @@
 #define EXTENDED_STD_XMEMCTL_H
 #include <stddef.h>
 #include <stdint.h>
+#define X_CONCAT_INTERNAL(a, b) a##b
+#define X_CONCAT(a, b) X_CONCAT_INTERNAL(a, b)
+
+#define XMEM_SCOPE \
+    for (int X_CONCAT(_scope_i_, __LINE__) = (xmem_checkpoint(), 0); \
+    X_CONCAT(_scope_i_, __LINE__) < 1; \
+    X_CONCAT(_scope_i_, __LINE__)++, xmem_rollback())
 
 typedef struct XMemStackNode {
     void *ptr;

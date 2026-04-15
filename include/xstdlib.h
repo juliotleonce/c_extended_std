60c833b1664fef7fcbff4f3c4ec233139e444406
JuliotLeonce <juliotleonce123@gmail.com>
2026-04-15 10:36:29 +0300

    edit(lib): change library name from extended_std to xstdlib

---
+++ include/xstdlib.h
@@ -0,0 +1,15 @@
+//
+// Created by ajl420 on 4/15/26.
+//
+
+#ifndef EXTENDED_STD_XSTDLIB_H
+#define EXTENDED_STD_XSTDLIB_H
+
+#include "xarray.h"
+#include "xhashmap.h"
+#include "xhashset.h"
+#include "xlinked_list.h"
+#include "xmemctl.h"
+#include "xstring.h"
+
+#endif //EXTENDED_STD_XSTDLIB_H

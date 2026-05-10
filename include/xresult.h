89e0674bd8e84d4f64b8c465454e9b1cf4138d00
JuliotLeonce <juliotleonce123@gmail.com>
2026-05-10 19:03:50 +0300

    feat(xresult): add macros for handling unsure results and error checking

---
+++ include/xresult.h
@@ -0,0 +1,55 @@
+#ifndef EXTENDED_STD_XRESULT_H
+#define EXTENDED_STD_XRESULT_H
+
+#define XResult(T) XResult_##T
+
+#define DEFINE_XRESULT(T) typedef struct { T value; int error; } XResult_##T;
+
+#define OK(T, val) ((XResult_##T){ .value = (val), .error = 0 })
+#define ERR(T, err_code) ((XResult_##T){ .value = {0}, .error = (err_code) })
+
+#define IS_OK(xresult) ({ \
+    __typeof__(xresult) _tmp = (xresult); \
+    _tmp.error == 0; \
+})
+
+#define IS_ERR(xresult) ({ \
+    __typeof__(xresult) _tmp = (xresult); \
+    _tmp.error != 0; \
+})
+
+#define UNWRAP(xresult) ({ \
+    __typeof__(xresult) _tmp = (xresult); \
+    if (_tmp.error != 0) { \
+        fprintf(stderr, "Panic: Unwrapped an error value (%d)\n", _tmp.error); \
+        exit(_tmp.error); \
+    } \
+    _tmp.value; \
+})
+
+#define UNWRAP_OR(xresult, default_value) ({ \
+    __typeof__(xresult) _tmp = (xresult); \
+    (_tmp.error == 0) ? _tmp.value : (default_value); \
+})
+
+#define PROPAGATE(xresult, T) do { \
+    __typeof__(xresult) _tmp = (xresult); \
+    if (_tmp.error != 0) return ERR(T, _tmp.error); \
+} while (0)
+
+#define EXPECT(xresult, msg) ({ \
+    __typeof__(xresult) _tmp = (xresult); \
+    if (_tmp.error != 0) { \
+        fprintf(stderr, "Fatal Error: %s (Code: %d)\n", msg, _tmp.error); \
+        exit(_tmp.error); \
+    } \
+    _tmp.value; \
+})
+
+#define TRY(xresult, T_CURRENT) ({ \
+    __typeof__(xresult) _tmp = (xresult); \
+    if (_tmp.error != 0) return ERR(T_CURRENT, _tmp.error); \
+    _tmp.value; \
+})
+
+#endif

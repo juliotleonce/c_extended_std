ae08463d46f03605f690341dcb44a8d8a2f32d9e
JuliotLeonce <juliotleonce123@gmail.com>
2026-05-10 19:55:30 +0300

    feat(xstdlib): add xio_utils and xresult header for lib principal header

--- include/xstdlib.h
+++ include/xstdlib.h
@@ -1,7 +1,3 @@
-//
-// Created by ajl420 on 4/15/26.
-//
-
 #ifndef EXTENDED_STD_XSTDLIB_H
 #define EXTENDED_STD_XSTDLIB_H
 
@@ -11,5 +7,7 @@
 #include "xlinked_list.h"
 #include "xmemctl.h"
 #include "xstring.h"
+#include "xresult.h"
+#include "xio_utils.h"
 
 #endif //EXTENDED_STD_XSTDLIB_H
